d141dacdddaba67ecf176d65c91d9fb27d1270b7
vamsy16 <vamsi2384@gmail.com>
2021-12-01 18:09:21 +0530

    add

--- SampleProject/src/MasterClass.java
+++ SampleProject/src/MasterClass.java
@@ -4,6 +4,7 @@ public class MasterClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("master class!");
+		System.out.println("hello!!");
 	}
 
 }
--- SampleProject/src/MasterClass5.java
+++
@@ -1,9 +0,0 @@
-
-public class MasterClass5 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
