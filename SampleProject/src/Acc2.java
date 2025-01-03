0a7383aedb1778484584a56f81f6078f4ea8a5ea
chandanasaro <saro.chandana@gmail.com>
2025-01-03 15:39:43 +0530

    my commit

---
+++ SampleProject/src/Acc2.java
@@ -0,0 +1,11 @@
+
+public class Acc2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Added!");
+		System.out.println("one more added");
+		System.out.println("added");
+	}
+
+}
