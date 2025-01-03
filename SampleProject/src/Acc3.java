59c9d04207bd19f07f7c286254cad69b479a76bb
Your Name <you@example.com>
2025-01-03 15:50:05 +0530

    committing

---
+++ SampleProject/src/Acc3.java
@@ -0,0 +1,11 @@
+
+public class Acc3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Added!");
+		System.out.println("one more added");
+		System.out.println("3 acc");
+	}
+
+}
---
+++ SampleProject/src/MasterClass3.java
@@ -0,0 +1,11 @@
+
+public class MasterClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("master class!");
+		System.out.println("hello!!");
+		System.out.println("3 class");
+	}
+
+}
